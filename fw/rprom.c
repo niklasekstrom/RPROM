793cbd7688851e2538ff67a8664d891c0a9d0ffd
Niklas Ekström <mail@niklasekstrom.nu>
2025-08-16 15:55:01 +0200

    Add slot config to flash

--- fw/rprom.c
+++ fw/rprom.c
@@ -1,6 +1,8 @@
 #include "hardware/clocks.h"
+#include "hardware/flash.h"
 #include "hardware/gpio.h"
 
+#include "pico/flash.h"
 #include "pico/multicore.h"
 
 #include <string.h>
@@ -14,11 +16,81 @@
 __attribute__((section(".rom_image")))
 static uint16_t rom_image[256 * 1024];
 
+#define ROM_SLOT_SIZE (512 * 1024)
+
+#define CONFIG_SECTOR_OFFSET (ROM_SLOT_SIZE - FLASH_SECTOR_SIZE)
+
+#define CONFIG_SECTOR_BASE (XIP_BASE + CONFIG_SECTOR_OFFSET)
+
+struct ConfigTuple
+{
+    uint32_t rom_slot;
+    uint32_t reserved;
+};
+
+struct ConfigPage
+{
+    uint32_t pages_bitmap;
+    uint32_t tuples_bitmap;
+    struct ConfigTuple tuples[31];
+};
+
+static inline uint32_t get_active_rom_slot()
+{
+    struct ConfigPage *pages = (struct ConfigPage *)CONFIG_SECTOR_BASE;
+    uint32_t active_page = __builtin_ctz(pages[0].pages_bitmap);
+    uint32_t active_tuple = __builtin_ctz(pages[active_page].tuples_bitmap);
+    uint32_t rom_slot = pages[active_page].tuples[active_tuple].rom_slot;
+    return (rom_slot == 0 || rom_slot >= 8) ? 1 : rom_slot;
+}
+
+static void update_active_rom_slot_in_flash(uint32_t rom_slot)
+{
+    struct ConfigPage *pages = (struct ConfigPage *)CONFIG_SECTOR_BASE;
+    uint32_t active_page = __builtin_ctz(pages[0].pages_bitmap);
+    uint32_t active_tuple = __builtin_ctz(pages[active_page].tuples_bitmap);
+
+    active_tuple++;
+    if (active_tuple >= 31)
+    {
+        active_tuple = 0;
+        active_page++;
+        if (active_page >= 16)
+        {
+            active_page = 0;
+            flash_range_erase(CONFIG_SECTOR_OFFSET, FLASH_SECTOR_SIZE);
+        }
+    }
+
+    // Uses 256 bytes of stack space.
+    struct ConfigPage page;
+    memcpy(&page, (const void *)&pages[active_page], sizeof(page));
+
+    page.pages_bitmap = ~((1 << active_page) - 1);
+    page.tuples_bitmap = ~((1 << active_tuple) - 1);
+    page.tuples[active_tuple].rom_slot = rom_slot;
+
+    flash_range_program(CONFIG_SECTOR_OFFSET + active_page * FLASH_PAGE_SIZE,
+                        (const uint8_t *)&page, FLASH_PAGE_SIZE);
+
+    if (active_page != 0)
+    {
+        memcpy(&page, (const void *)&pages[0], FLASH_PAGE_SIZE);
+        page.pages_bitmap = ~((1 << active_page) - 1);
+        flash_range_program(CONFIG_SECTOR_OFFSET, (const uint8_t *)&page, FLASH_PAGE_SIZE);
+    }
+}
+
 static void handle_magic_read(uint32_t address)
 {
-    const uint32_t slot = address & 7;
-    const uint32_t slot_address = 0x10000000 + slot * 0x80000;
-    memcpy(rom_image, (const void *)slot_address, sizeof(rom_image));
+    uint32_t rom_slot = address;
+    if (rom_slot == 0 || rom_slot >= 8)
+        return;
+
+    update_active_rom_slot_in_flash(rom_slot);
+
+    uint32_t rom_slot_base = XIP_BASE + rom_slot * ROM_SLOT_SIZE;
+    memcpy(rom_image, (const void *)rom_slot_base, sizeof(rom_image));
 }
 
 static void __not_in_flash_func(core1_main)()
@@ -63,7 +135,7 @@ static inline void multicore_fifo_push_non_blocking_inline(uint32_t data) {
     __sev();
 }
 
-static void __not_in_flash_func(rom_emulation)()
+static void __not_in_flash_func(core0_main)()
 {
     uint32_t triggered = 0;
 
@@ -98,14 +170,14 @@ void __not_in_flash_func(main)()
 {
     set_sys_clock_khz(200000, false);
 
-    const uint32_t slot = 1;
-    const uint32_t slot_address = 0x10000000 + slot * 0x80000;
-    memcpy(rom_image, (const void *)slot_address, sizeof(rom_image));
+    const uint32_t rom_slot = get_active_rom_slot();
+    const uint32_t rom_slot_base = XIP_BASE + rom_slot * ROM_SLOT_SIZE;
+    memcpy(rom_image, (const void *)rom_slot_base, sizeof(rom_image));
 
     for (uint i = 0; i < 37; i++)
         gpio_init(i);
 
-    multicore_launch_core1(core1_main);
+    multicore_launch_core1_with_stack(core1_main, (uint32_t *)0x20081400, 2*1024);
 
-    rom_emulation();
+    core0_main();
 }
